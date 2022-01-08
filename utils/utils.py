a0289a5ab7317920f8830944d27e4b196c79c71e
Eugene Wang <eugenew@spotify.com>
2022-01-07 17:42:29 -0800

    Create common.py containing static shared game information

--- utils/utils.py
+++ utils/utils.py
@@ -1,12 +1,10 @@
-import urllib.request
 import re
+
+from collections import defaultdict
+from utils.common import NUM_LETTERS, COLORS
 from typing import List, Dict
 
-english_dictionary = [word for word in urllib.request.urlopen(
-    "https://www-cs-faculty.stanford.edu/~knuth/sgb-words.txt").read().decode().split("\n") if len(word) == 5]
-
-
-def _check_green(input_word: str, target_word: str) -> List[int]:
+def __check_green(input_word: str, target_word: str) -> List[int]:
     """
     input_word: "balls"
     target_word: "blues"
@@ -27,9 +25,9 @@ def get_letter_colors(input_word: str, target_word: str) -> Dict:
 
     :param input_word: string of word to check
     :param target_word: string of word to check against
-    :return: dictionary of color to list of indices
+    :return: dictionary of letter to color to list of indices
     """
-    green_indices = _check_green(input_word, target_word)
+    green_indices = __check_green(input_word, target_word)
 
     #### color indices
     letter_dict = {}
@@ -39,30 +37,30 @@ def get_letter_colors(input_word: str, target_word: str) -> Dict:
         input_word_list[index] = "_"
         target_word_list[index] = "_"
         if input_word[index] not in letter_dict:
-            letter_dict[input_word[index]] = {"green": [index]}
+            letter_dict[input_word[index]] = {COLORS.green : [index]}
         else:
-            letter_dict[input_word[index]]["green"] += [index]
+            letter_dict[input_word[index]][COLORS.green] += [index]
 
     for ind, character in enumerate(input_word_list):
         if character == "_":
             continue
         if character in letter_dict:
             if character not in target_word_list:
-                if "grey" in letter_dict[character]:
-                    letter_dict[character]["grey"] += [ind]
+                if COLORS.grey in letter_dict[character]:
+                    letter_dict[character][COLORS.grey] += [ind]
                 else:
-                    letter_dict[character]["grey"] = [ind]
+                    letter_dict[character][COLORS.grey] = [ind]
             else:
-                if "yellow" in letter_dict[character]:
-                    letter_dict[character]["yellow"] += [ind]
+                if COLORS.yellow in letter_dict[character]:
+                    letter_dict[character][COLORS.yellow] += [ind]
                 else:
-                    letter_dict[character]["yellow"] = [ind]
+                    letter_dict[character][COLORS.yellow] = [ind]
                 target_word_list[target_word_list.index(character)] = "_"
         else:
             if character not in target_word_list:
-                letter_dict[character] = {"grey": [ind]}
+                letter_dict[character] = {COLORS.grey: [ind]}
             else:
-                letter_dict[character] = {"yellow": [ind]}
+                letter_dict[character] = {COLORS.yellow: [ind]}
                 target_word_list[target_word_list.index(character)] = "_"
     return letter_dict
 
