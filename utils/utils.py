a14e08b29d07f64c0bc4df0154d2a10a575ccc35
Abhijit Brahme <abhijitbrahme@Abhijits-MacBook-Pro.local>
2022-01-08 13:04:05 -0800

    csv with more words

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,7 @@
 from typing import List
 
 with open("utils/words.txt",'rb') as f:
-    english_dictionary = [word for word in f.read().decode().strip().split("\n") if len(word) == 5]
+    english_dictionary = [word for word in f.read().decode().strip().split(",") if len(word) == 5]
 f.close()
 
 
